abcc25dce00e4fd25f016d3951b05ee0ea6b91cf
banosdiego <diegobanos@hotmail.com>
2023-05-02 08:29:19 -0600

    morning commit

---
+++ Excercise2.py
@@ -0,0 +1,10 @@
+class Cylinder:
+    def __init__(self,radius=1,height=1):
+        self.radius=radius
+        self.height=height
+
+    def volume(self):
+        pass
+
+    def surface_area(self):
+        pass
--- Exercise1.py
+++ Exercise1.py
@@ -9,11 +9,15 @@ class Line:
         x2,y2 = coordinate2
         return ((((x2 - x1)**2) + ((y2 - y1)**2))**(1/2))
 
-    def slope():
-        pass
+    def slope(self):
+        x1,y1 = coordinate1
+        x2,y2 = coordinate2
+        return (y2-y1)/(x2-x1)
+    
 coordinate1=(3,2)
 coordinate2=(8,10)
 
 l = Line(coordinate1,coordinate2)
 a = l.distance()
-print(a)
+b = l.slope()
+print(b)
